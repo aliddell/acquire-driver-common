002e2af0342cdf354fa84def3731477b2a0af842
Alan Liddell <aliddell@chanzuckerberg.com>
2023-05-04 16:36:33 -0400

    Reenable simd.

--- cmake/simd.cmake
+++ cmake/simd.cmake
@@ -1,13 +1,12 @@
 include(cmake/TargetArch.cmake)
 
 function(target_enable_simd tgt)
-    message("SIMD is temporarily disabled. See https://github.com/acquire-project/acquire-driver-common/issues/7")
-#    target_architecture(arch)
-#    set(is_gcc_like "$<OR:$<COMPILE_LANG_AND_ID:CXX,AppleClang,Clang,GNU>,$<COMPILE_LANG_AND_ID:C,AppleClang,Clang,GNU>>")
-#    set(is_msvc_like "$<OR:$<COMPILE_LANG_AND_ID:CXX,MSVC>,$<COMPILE_LANG_AND_ID:C,MSVC>>")
-#    set(is_arch_x64 "$<STREQUAL:${arch},x86_64>")
-#    target_compile_options(${tgt} PRIVATE
-#            $<$<AND:${is_arch_x64},${is_gcc_like}>:-mavx2>
-#            $<$<AND:${is_arch_x64},${is_msvc_like}>:/arch:AVX2>
-#            )
+    target_architecture(arch)
+    set(is_gcc_like "$<OR:$<COMPILE_LANG_AND_ID:CXX,AppleClang,Clang,GNU>,$<COMPILE_LANG_AND_ID:C,AppleClang,Clang,GNU>>")
+    set(is_msvc_like "$<OR:$<COMPILE_LANG_AND_ID:CXX,MSVC>,$<COMPILE_LANG_AND_ID:C,MSVC>>")
+    set(is_arch_x64 "$<STREQUAL:${arch},x86_64>")
+    target_compile_options(${tgt} PRIVATE
+            $<$<AND:${is_arch_x64},${is_gcc_like}>:-mavx2>
+            $<$<AND:${is_arch_x64},${is_msvc_like}>:/arch:AVX2>
+            )
 endfunction()
